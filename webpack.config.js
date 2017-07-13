0e749e7c7b66f8e488b59fdea07992f8625c4ea1
Ernst Haagsman <ernst.haagsman@jetbrains.com>
2017-07-13 16:31:51 +0200

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+      { test: /\.css$/, use: [
+        {
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader',
+          options: { sourceMap: true }
+        }
+        ] }
+    ]
+  }
+};
